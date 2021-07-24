b9dc466af1cff01676632631a0c93804777e77f3
James Andrews <jandrews48301@yahoo.com>
2021-07-24 02:29:18 -0700

    navbar attempt and header stying

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import './Header.css';
 
 
 
 const Header = ({ title }) => (
     <div>
-      <a class="active" href="#">Home</a>
-      <a href="/trainers">Trainers</a>
-      <a href="/users">Users</a>
-      <a href="/admin">Admin</a>
-      <a href="/login">Login</a>
-      <a href="/signup">Signup</a>
+      <a id="1" class="active" href="#">Home</a>
+      <a  id="2" href="/trainers">Trainers</a>
+      <a id="3" href="/users">Users</a>
+      <a id="4" href="/admin">Admin</a>
+      <a id="5" href="/login">Login</a>
+      <a id="6" href="/signup">Signup</a>
       <h1>{title}</h1>
     </div>
 
