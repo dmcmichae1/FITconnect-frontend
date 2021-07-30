3bd3a221b0ca595b72cca174dcae8627139afa25
Joseph Wilson <josephw1996753@gmail.com>
2021-07-30 18:09:16 -0500

    added login and signup page

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -4,15 +4,33 @@ import React from 'react';
 const Signup = () => (
 
 
-     
+  <div className="signup">
+  <h2>Signup bro!</h2>
+  <form action="/action_page.php">
+  <div className="form-group">
+      <label for="firstName">First Name:</label>
+      <input type="firstName" className="form-control" id="firstName" placeholder="Enter first name" name="firstName"></input>
+    </div>
+    <div className="form-group">
+      <label for="lastName">Last Name:</label>
+      <input type="lastName" className="form-control" id="lastName" placeholder="Enter last name" name="lastName"></input>
+    </div>
+    <div className="form-group">
+      <label for="email">Username:</label>
+      <input type="email" className="form-control" id="email" placeholder="Enter username" name="email"></input>
+    </div>
+    <div className="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" className="form-control" id="pwd" placeholder="Enter password" name="pwd"></input>
+    </div>
+    <button type="submit" class="btn btn-default">Signup</button>
+  </form>
+</div>
     
    
   
 
-  <div>
-      
-     Hi I am signup
-     </div>
+  
 );
 
 export default Signup;
