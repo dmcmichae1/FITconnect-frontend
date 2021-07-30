52dbcefccf63fa68c2911db9f541bb706510a1ca
dmcmichae1 <mcmichaeldevin@icloud.com>
2021-07-30 12:50:52 -0700

    fixed errors

--- src/App.js
+++ src/App.js
@@ -3,23 +3,23 @@ import './App.css';
 import Home from './Pages/Home';
 import Trainer from './Pages/Trainer';
 import Signup from './Pages/Signup';
- import Login from './Pages/Login';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Login from './Pages/Login';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
 
 
 function App() {
-    return (
-        <Router>
-        <div>
-         <Route path="/home" component={Home} />
-         <Route path="/trainer" component={Trainer} />
-         <Route path="/signup" component={Signup} />
-         <Route path="/login" component={Login} />
-       </div> 
-     </Router>
-    );
+  return (
+    <Router>
+      <div>
+        <Route path="/home" component={Home} />
+        <Route path="/trainer" component={Trainer} />
+        <Route path="/signup" component={Signup} />
+        <Route path="/login" component={Login} />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
