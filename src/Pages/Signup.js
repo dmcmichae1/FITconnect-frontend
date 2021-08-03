e0a97a3f9536b3d970b3dd0a82a33a319aad637a
GabrielCabrera4950 <cabreragapc@gmail.com>
2021-08-03 14:17:08 -0700

    signupdate

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,36 +1,65 @@
 import React from 'react';
+import axios from 'axios';
+import { Route } from 'react-router-dom';
+import { withRouter } from 'react-router';
+import { useState } from 'react';
 
 
-const Signup = () => (
-
-
-  <div className="signup">
-    <h2>Signup bro!</h2>
-    <form action="/action_page.php">
-      <div className="form-group">
-        <label for="firstName">First Name:</label>
-        <input type="firstName" className="form-control" id="firstName" placeholder="Enter first name" name="firstName"></input>
-      </div>
-      <div className="form-group">
-        <label for="lastName">Last Name:</label>
-        <input type="lastName" className="form-control" id="lastName" placeholder="Enter last name" name="lastName"></input>
-      </div>
+   const SignUp = ({ history }) => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [firstName, setFirstname] = useState('');
+    const [lastName, setLastname] = useState('');
+  
+  
+  
+    const signUp = (e) => {
+      e.preventDefault();
+  
+      if (username !== '' && password !== '') {
+        const req = {
+          firstName: firstName,
+          lastName: lastName,
+          userName: username,
+          password: password
+        };
+  
+        axios.post('http://localhost:3000/trainers/signup', req).then(result => {
+          const token = result.data.jwt;
+          localStorage.setItem('myJWT', token);
+          history.push('/trainers');
+  
+          console.log(result.data);
+  
+        })
+       }
+    };
+  
+    return (<div className="login">
+    <h1>Sign Up Bro!</h1>
+    <form onSubmit={signUp} action="/action_page.php">
       <div className="form-group">
         <label for="username">Username:</label>
-        <input type="username" className="form-control" id="username" placeholder="Enter username" name="username"></input>
+        <input type="text" className="form-control" id="username" placeholder="Enter username" name="username" onChange={e => setUsername(e.target.value)}></input>
       </div>
       <div className="form-group">
         <label for="pwd">Password:</label>
-        <input type="password" className="form-control" id="pwd" placeholder="Enter password" name="pwd"></input>
+        <input type="password" className="form-control" id="pwd" placeholder="Enter password" name="pwd" onChange={e => setPassword(e.target.value)}></input>
       </div>
-      <button type="submit" class="btn btn-default">Signup</button>
+      <div className="form-group">
+        <label for="firstName">First Name:</label>
+        <input type="text" className="form-control" id="firstName" placeholder="Enter firstName" name="firstName" onChange={e => setFirstname(e.target.value)}></input>
+      </div>
+      <div className="form-group">
+        <label for="lastName">Last Name:</label>
+        <input type="lastName" className="form-control" id="lastName" placeholder="Enter lastName" name="lastName" onChange={e => setLastname(e.target.value)}></input>
+      </div>
+      <button type="submit" class="btn btn-default">Create</button>
     </form>
-  </div>
+  </div>);
 
+}
 
+ 
 
-
-
-);
-
-export default Signup;
+export default withRouter(SignUp);
--- src/Pages/Trainer.js
+++ src/Pages/Trainer.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Nav from './Navbar';
+import { withRouter } from "react-router";
 
 const Trainer = () => (
-
+   
   <div>
-    <Nav />
-    hi I am Trainer
-  </div>
-
+  <Nav />
+  Meet Our Trainers!!
+</div>
+  
 );
 
-export default Trainer;
+export default withRouter(Trainer);
